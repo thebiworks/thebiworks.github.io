3f80be5b4fcd05ec3109a192b35d9e91fcfcd309
Maouriyan <nibirule@gmail.com>
2019-01-16 16:48:42 +0530

    matiachu

--- js/contact-form-script.js
+++ js/contact-form-script.js
@@ -1,8 +1,6 @@
 $("#contactForm").validator().on("submit", function (event) {
     if (event.isDefaultPrevented()) {
         // handle the invalid form...
-        formError();
-        submitMSG(false, "Did you fill in the form properly?");
     } else {
         // everything looks good!
         event.preventDefault();
@@ -10,15 +8,12 @@ $("#contactForm").validator().on("submit", function (event) {
     }
 });
 
-
 function submitForm(){
     // Initiate Variables With Form Content
     var name = $("#name").val();
     var email = $("#email").val();
-    var msg_subject = $("#msg_subject").val();
     var message = $("#message").val();
-
-
+ 
     $.ajax({
         type: "POST",
         url: "php/form-process.php",
@@ -26,30 +21,10 @@ function submitForm(){
         success : function(text){
             if (text == "success"){
                 formSuccess();
-            } else {
-                formError();
-                submitMSG(false,text);
             }
         }
     });
 }
-
 function formSuccess(){
-    $("#contactForm")[0].reset();
-    submitMSG(true, "Message Submitted!")
-}
-
-function formError(){
-    $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-        $(this).removeClass();
-    });
-}
-
-function submitMSG(valid, msg){
-    if(valid){
-        var msgClasses = "h3 text-center tada animated text-success";
-    } else {
-        var msgClasses = "h3 text-center text-danger";
-    }
-    $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
+    $( "#msgSubmit" ).removeClass( "hidden" );
 }
